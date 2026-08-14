1d98905ca1ae3ff197ec2fcc5a0100d9fd3b46f7
kevinhou7447@gmail.com <104533338+Toastyybread1@users.noreply.github.com>
2026-08-14 15:54:39 -0400

    connect gemini api, embed chunks, add Supabase key

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
+#pdf convertor stuff 
 from fastapi import FastAPI, UploadFile, File
 import pypdf
 import io
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+#gemini stuff 
+import os
+from google import genai
+from dotenv import load_dotenv 
+
 app = FastAPI()
+
+load_dotenv() #load enviroment vars from .env file
+ai = genai.client(api_key=os.getenv("GEMINI_API_KEY")) 
+embeddings = [] 
+
 
 #set the endpoint for uploading a PDF file
 @app.post("/upload")
@@ -25,7 +36,6 @@ async def upload_pdf(file: UploadFile = File(...)):
             full_text += extracted + "\n"
 
     # 4. Split text into chunks
-    # This strategy provides a solid balance between keeping context intact and managing chunk size
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, 
         chunk_overlap=200, #the chunks seperated need an overlap (chunk 2 gets the ending 200 chars of chunk 1) to maintain context for the LLM
@@ -34,7 +44,14 @@ async def upload_pdf(file: UploadFile = File(...)):
     chunks = text_splitter.split_text(full_text)
 
     for chunk in chunks:
-        print(f'Chunk: {chunks.index(chunk) + 1}\n{chunk}')
+        response = ai.models.embed_content (
+            model="gemini-embedding-001",
+            contents = chunk
+        )
+    embeddings.append(response.embeddings.values)
+
+
+        
     # 5. Return success and metadata
     return {
         "filename": file.filename,
